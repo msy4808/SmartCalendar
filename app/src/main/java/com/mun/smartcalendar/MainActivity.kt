e09515d25ecaa72be18b7538ce8a0e3cf4f05899
msy4808 <mkik90911@naver.com>
2024-10-19 01:18:02 +0900

    Bottom bar basic UI implementation :: To be modified

--- app/src/main/java/com/mun/smartcalendar/MainActivity.kt
+++ app/src/main/java/com/mun/smartcalendar/MainActivity.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.mun.smartcalendar.ui.theme.SmartCalendarTheme
+import com.mun.smartcalendar.ui.theme.SplashBackGround
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +31,36 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SmartCalendarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        BottomAppBar(Modifier.background(SplashBackGround)) {
+                            IconButton(
+                                onClick = {},
+                                Modifier.weight(1f)
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Filled.DateRange,
+                                    contentDescription = null
+                                )
+                            }
+                            IconButton(
+                                onClick = {},
+                                Modifier.weight(1f)
+                            ) {
+                                Icon(imageVector = Icons.Filled.Search, contentDescription = null)
+                            }
+                            IconButton(
+                                onClick = {},
+                                Modifier.weight(1f)
+                            ) {
+                                Icon(imageVector = Icons.Filled.Info, contentDescription = null)
+                            }
+                        }
+                    }) { innerPadding ->
+                    MainScreen(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .background(Color.White)
                     )
                 }
             }
@@ -31,17 +69,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(modifier: Modifier = Modifier) {
+    Box(modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center) {
+        Text(text = "Test")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SmartCalendarTheme {
-        Greeting("Android")
+        MainScreen(modifier = Modifier)
     }
 }
